4b1e5300cbcbd442f1eb4d5151f8d4db20a48fd1
abdullayev ulvi <abdullayevulvi492@gmail.com>
2021-11-11 13:42:16 +0400

    tuple ve listin ferqini kodlar uzerinde gosterdim

---
+++ Desktop/number/app.py
@@ -0,0 +1,12 @@
+#fennler=['riyaz','rus','ingilis']
+#fennler.append('alman')
+#print(fennler)  
+
+#fennler=('riyaz','rus','ingilis')
+#fennler.append('alman')
+#print(fennler)
+
+fennler=['riyaz','rus','ingilis']
+fennler[2]='informatika'
+print(fennler)
+
